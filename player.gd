6b7179d058975348e55f55f7caccad76b84c69f3
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-09-08 02:50:26 +0200

    Update demo to work on the latest latest Godot `master` branch

--- player.gd
+++ player.gd
@@ -15,7 +15,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		var rot_x := pivot.rotation.x - mm.relative.y * rot_speed
 		rot_x = clamp(rot_x, -0.25 * TAU, 0.25 * TAU)
 		var rot_y := pivot.rotation.y - mm.relative.x * rot_speed
-		pivot.transform.basis = Basis(Vector3(rot_x, rot_y, 0))
+		pivot.transform.basis = Basis.from_euler(Vector3(rot_x, rot_y, 0))
 
 	if event is InputEventKey and event.keycode == KEY_ESCAPE:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -26,17 +26,17 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _physics_process(_delta: float) -> void:
 	# Apply friction.
-	linear_velocity.x *= 0.96
-	linear_velocity.y *= 0.98
-	linear_velocity.z *= 0.96
+	velocity.x *= 0.96
+	velocity.y *= 0.98
+	velocity.z *= 0.96
 
 	# Apply gravity.
-	linear_velocity.y -= 0.5
+	velocity.y -= 0.5
 
 	# Player movement.
 	var motion := Vector3()
 	motion.x += Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	motion.z += Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	const move_speed = 1.5
-	linear_velocity += motion.rotated(Vector3(0, 1, 0), pivot.rotation.y) * move_speed
+	velocity += motion.rotated(Vector3(0, 1, 0), pivot.rotation.y) * move_speed
 	move_and_slide()
